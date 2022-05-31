56e8be0bab0274cda24a458e44f7db768449c424
sysionng <sysionng@gmail.com>
2022-05-31 16:29:49 +0100

    Resolved show/hide menu when mobile-menu is clicked

--- js/main.js
+++ js/main.js
@@ -3,12 +3,55 @@ let ul_menu = document.querySelector('nav ul');
 
 mobile_menu.addEventListener('click', (event) => {
 	event.preventDefault();
+	event.stopPropagation();
 
+	/* NOT working properly. Shows menu but not able to hide it.
+	console.log('before: ul_menu display style = '+ window.getComputedStyle(ul_menu).display);
+	console.log('before: ul_menu display style2 = '+ ul_menu.style.display);
 	if (ul_menu.style.display === '' || ul_menu.style.display === 'none'){
-		ul_menu.style.display = 'flex';
+		//ul_menu.style.display = 'flex';
+		ul_menu.classList.remove('remove-menu');
+		setTimeout(()=>{
+			ul_menu.classList.add('show-menu');
+			setTimeout(()=>{
+				ul_menu.classList.remove('hide-menu');
+			}, 20);
+		}, 20);
+
+		console.log('ul_menu classes = '+ ul_menu.classList);
+		console.log('ul_menu display style = '+ window.getComputedStyle(ul_menu).display);
 	}
-	else if (ul_menu.style.display === 'flex'){
-		ul_menu.style.display = 'none';
+	//else if (ul_menu.style.display === 'flex'){
+	else if (window.getComputedStyle(ul_menu).display === 'flex'){
+		console.log(1);
+		//ul_menu.style.display = 'none';
+		ul_menu.classList.add('hide-menu');
+		setTimeout(()=>{
+			ul_menu.classList.remove('show-menu');
+			setTimeout(()=>{
+				ul_menu.classList.add('remove-menu');
+			}, 20);
+		}, 20);
+	}
+	*/
+
+	if (window.getComputedStyle(ul_menu).display === '' || window.getComputedStyle(ul_menu).display === 'none'){
+		ul_menu.classList.remove('remove-menu');
+		setTimeout(()=>{
+			ul_menu.classList.add('show-menu');
+			setTimeout(()=>{
+				ul_menu.classList.remove('hide-menu');
+			}, 20);
+		}, 20);
+	}
+	else if (window.getComputedStyle(ul_menu).display === 'flex'){
+		ul_menu.classList.remove('show-menu');
+		setTimeout(()=>{
+			ul_menu.classList.add('hide-menu');
+		}, 20);
+		setTimeout(()=>{
+			ul_menu.classList.add('remove-menu');
+		}, 500);
 	}
 });
 
@@ -30,9 +73,19 @@ window.addEventListener('orientationchange', (event) => {
 //window.addEventListener('load', (event) => {
 window.addEventListener('DOMContentLoaded', (event) => {
 	if (window.getComputedStyle(mobile_menu).display === 'block'){
-		ul_menu.style.display = 'none';
+		//ul_menu.style.display = 'none';
+		if (! ul_menu.classList.contains('remove-menu')){
+			ul_menu.classList.add('remove-menu');
+			ul_menu.classList.add('hide-menu');
+			ul_menu.classList.remove('show-menu');
+		}
 	}
 	else{
-		ul_menu.style.display = 'flex';
+		//ul_menu.style.display = 'flex';
+		if (ul_menu.classList.contains('remove-menu')){
+			ul_menu.classList.remove('remove-menu');
+			ul_menu.classList.remove('hide-menu');
+			ul_menu.classList.add('show-menu');
+		}
 	}
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # Store Landing Page
+
+A simple html/css/javascript responsive landing page for a fictitious store
+called BigStore.
+
+CSS flexbox and grid used to ensure proper placing of element on page.
