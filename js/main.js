52f97ed1039ea04884e31073d8450e3af24d01cc
sysionng <sysionng@gmail.com>
2022-05-31 23:01:11 +0100

    Clean up after changes to index.js

--- js/main.js
+++ js/main.js
@@ -32,17 +32,13 @@ window.addEventListener('orientationchange', (event) => {
 
 window.addEventListener('DOMContentLoaded', (event) => {
 	if (window.getComputedStyle(mobile_menu).display === 'block'){
-		//if (! ul_menu.classList.contains('remove-menu')){
-			ul_menu.classList.add('remove-menu');
-			ul_menu.classList.add('hide-menu');
-			ul_menu.classList.remove('show-menu');
-		//}
+		ul_menu.classList.add('remove-menu');
+		ul_menu.classList.add('hide-menu');
+		ul_menu.classList.remove('show-menu');
 	}
 	else{
-		//if (ul_menu.classList.contains('remove-menu')){
-			ul_menu.classList.remove('remove-menu');
-			ul_menu.classList.remove('hide-menu');
-			ul_menu.classList.add('show-menu');
-		//}
+		ul_menu.classList.remove('remove-menu');
+		ul_menu.classList.remove('hide-menu');
+		ul_menu.classList.add('show-menu');
 	}
 });
